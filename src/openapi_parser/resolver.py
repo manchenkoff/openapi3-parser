f5c827e2c36bf6deefbc6fe1c300b92716a8b44a
Andrew-Chen-Wang <acwangpython@gmail.com>
2023-04-19 11:24:32 -0400

    Allow passing string to load spec

--- src/openapi_parser/resolver.py
+++ src/openapi_parser/resolver.py
@@ -12,9 +12,10 @@ logger = logging.getLogger(__name__)
 class OpenAPIResolver:
     _resolver: prance.ResolvingParser
 
-    def __init__(self, uri: str) -> None:
+    def __init__(self, uri: str | None, spec_string: str | None) -> None:
         self._resolver = prance.ResolvingParser(
             uri,
+            spec_string=spec_string,
             backend=OPENAPI_SPEC_VALIDATOR,
             strict=False,
             lazy=True
