d14635ad25341547e0bde2b0db74c151517eba5a
manchenkoff <artem@manchenkoff.me>
2025-09-21 20:46:11 +0100

    feat: migrate to uv from pipenv

--- src/openapi_parser/__init__.py
+++ src/openapi_parser/__init__.py
@@ -1,11 +1,3 @@
-__version__ = '1.1.21'
-__title__ = 'openapi3-parser'
-__author__ = 'Artem Manchenkov'
-__email__ = 'artem@manchenkoff.me'
-__description__ = 'OpenAPI v3 parser'
-
 from .parser import parse
 
-__all__ = [
-    "parse"
-]
+__all__ = ["parse"]
